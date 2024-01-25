02bdef873d4acdc4c2d4ea3023f573f871298fa6
mjkim1225 <01026599478a@gmail.com>
2024-01-25 18:02:44 +0900

    글 삭제

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,10 +1,12 @@
 "use client"
 import Link from 'next/link';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
-export function Control(props) {
+export function Control() {
   const params = useParams();
   const id = params.id;
+
+  const router = useRouter();
 
   return (
     <ul>
@@ -13,7 +15,16 @@ export function Control(props) {
         id
           ? <>
             <li><Link href={"/update/"+id}>Update</Link></li>
-            <li><input type="button" value="delete"></input></li>
+            <li><input type="button" value="delete" onClick={() => {
+              fetch('http://localhost:9999/topics/'+id, {
+                method: 'DELETE',
+              }).then(res => res.json())
+              .then(data => {
+                console.log(data);
+                router.push('/');
+                router.refresh();
+              })
+            }}></input></li>
           </>
           : null
       }
