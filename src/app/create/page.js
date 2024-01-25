328a6eaa1dcdde6f55d2e9884f3ff050e3af964d
mjkim1225 <01026599478a@gmail.com>
2024-01-25 17:12:32 +0900

    글 생성하기

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,7 +1,37 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
+
 export default function Create() {
+    const router = useRouter();
 return (
-        <div>
-        <h2>Create</h2>
-        </div>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            const title = e.target.title.value;
+            const body = e.target.body.value;
+            fetch('http://localhost:9999/topics', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({title, body}),
+            }).then(res => res.json())
+            .then(data => {
+                console.log(data);
+                router.push(`/read/${data.id}`);
+            })
+
+        }}>
+            <p>
+                <input type="text" name="title" placeholder="title"></input>
+            </p>
+            <p>
+                <textarea name="body" placeholder="body"></textarea>
+            </p>
+            <p>
+                <input type="submit" value="create"></input>
+            </p>
+        </form>
     );
 }
